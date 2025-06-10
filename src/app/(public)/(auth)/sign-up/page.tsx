a17f5347beeab7f7aa255f43816eed7694fb1aa3
justuche224 <justuche224@gmail.com>
2025-06-10 20:26:28 +0100

    update: disable sign up

--- src/app/(public)/(auth)/sign-up/page.tsx
+++ src/app/(public)/(auth)/sign-up/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/auth/sign-up";
+// import { RegisterForm } from "@/components/auth/sign-up";
 import { info } from "@/constants";
 import { serverAuth } from "@/lib/server-auth";
 import { Metadata } from "next";
@@ -19,7 +19,16 @@ const page = async () => {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
-            <RegisterForm />
+            {/* <RegisterForm /> */}
+            {/* we are not onboarding user currently */}
+            <h1 className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-pink-600 text-2xl font-bold text-center">
+              Hey, Happy you want to try out zynkart but we are currently not
+              onboarding users.
+            </h1>
+            <p className="text-center text-muted-foreground">
+              Follow us on social medias to be the first to know when sign up is
+              available!
+            </p>
           </div>
         </div>
       </div>
